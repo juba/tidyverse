7d8ce29d77a303c63f0458550bbba8a9bc457c8f
Julien Barnier <julien@nozav.org>
2018-03-01 21:46:52 +0100

    Amélioration de l'affichage des solutions d'exercices

--- js/tidyverse.js
+++ js/tidyverse.js
@@ -1,11 +1,12 @@
 (function() {
   
   $(".solution").each(function() {
-    $(this).before("<p class='solution-link'><a href='#'>Solution</a></p>");
+    $(this).before("<p class='solution-link'><a href='#'>la solution</a></p>");
     $(this).hide();
     console.log($(this).prev());
     $(this).prev().on("click", function() {
       $(this).next().slideToggle();
+      $(this).toggleClass("shown");
     });
     
   });
