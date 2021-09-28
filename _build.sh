f7d4bf5762f698e17a6f5fd30734766e26651365
Julien <julien@nozav.org>
2021-09-28 13:01:06 +0200

    Create build script for gitbook format only

--- _build.sh
+++ _build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
